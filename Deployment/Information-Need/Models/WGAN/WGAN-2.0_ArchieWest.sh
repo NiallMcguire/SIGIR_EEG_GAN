3b8994d856ae152fb5beb034d4c7326f63791725
NiallMcguire <mundanelarch@gmail.com>
2023-12-12 13:40:55 +0000

    weighted augmentation for lists

--- Deployment/Information-Need/Models/WGAN/WGAN-2.0_ArchieWest.sh
+++ Deployment/Information-Need/Models/WGAN/WGAN-2.0_ArchieWest.sh
@@ -43,7 +43,7 @@ module load anaconda/python-3.9.7/2021.11
 #Modify the line below to run your program. This is an example
 #=========================================================
  
-python /users/gxb18167/SIGIR_EEG_GAN/Deployment/EEG-To-Text-GAN/Models/WGAN/WGAN-2.0.py
+python /users/gxb18167/SIGIR_EEG_GAN/Deployment/Information-Need/Models/WGAN/WGAN-2.0.py
 
 # Do not change the line below
 #=========================================================
