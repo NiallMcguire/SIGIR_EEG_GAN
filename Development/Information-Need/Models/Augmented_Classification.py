d93e511efbb1e92587259db09b7156fa580d02a6
NiallMcguire <mundanelarch@gmail.com>
2023-12-14 09:54:00 +0000

    weighted augmentation for lists

--- Development/Information-Need/Models/Augmented_Classification.py
+++ Development/Information-Need/Models/Augmented_Classification.py
@@ -1,7 +1,13 @@
 import pickle
+from collections import Counter
+from math import floor
+
+import numpy as np
+import seaborn as sns
 import pandas as pd
 import torch
-from sklearn.metrics import precision_recall_fscore_support, accuracy_score
+from matplotlib import pyplot as plt
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score, classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from torch import nn
@@ -29,9 +35,10 @@ def get_selected_features(SearchFeatures, include_segments=2):
 
     return all_selected_eeg
 
-def get_search_x_y(SearchFeatures, label, augmentation_factor=0, gen_model=None):
+def get_search_x_y(SearchFeatures, label, augmentation_factor=0):
     X_data = []
     Y_Data = []
+
 
     for index in range(len(SearchFeatures)):
         sentence_eeg = SearchFeatures[index]
@@ -40,23 +47,30 @@ def get_search_x_y(SearchFeatures, label, augmentation_factor=0, gen_model=None)
             X_data.append(eeg)
             Y_Data.append(label)
 
-    if augmentation_factor > 0:
-        augmentation_size = len(X_data)/100*augmentation_factor
-        print("Augmentation Size:", augmentation_size)
-        for i in range(int(augmentation_size)):
-            synthetic_eeg = gs.generate_synthetic_samples(gen_model=gen_model, EEG_word_level_embeddings=X_data)
-            X_data.append(synthetic_eeg)
-            Y_Data.append(label)
+
     return X_data, Y_Data
 
-def combine_data(NeedToSearch_X, CorrectSearch_X, IncorrectSearch_X, NeedToSearch_Y, CorrectSearch_Y, IncorrectSearch_Y):
-    X_data = NeedToSearch_X + CorrectSearch_X[:len(NeedToSearch_X)] + IncorrectSearch_X[:len(NeedToSearch_X)]
-    Y_data = NeedToSearch_Y + CorrectSearch_Y[:len(NeedToSearch_X)] + IncorrectSearch_Y[:len(NeedToSearch_X)]
+def combine_data(NeedToSearch_X, CorrectSearch_X, IncorrectSearch_X, NeedToSearch_Y, CorrectSearch_Y, IncorrectSearch_Y, augmentation_len):
+
+    equal_size = floor((len(NeedToSearch_X)/2+augmentation_len))
+
+    X_data = NeedToSearch_X + CorrectSearch_X[:equal_size] + IncorrectSearch_X[:equal_size]
+    Y_data = NeedToSearch_Y + CorrectSearch_Y[:equal_size] + IncorrectSearch_Y[:equal_size]
+
     return X_data, Y_data
 
-def get_all_subject_x_y(data, include_segments=2, augmentation_factor=0, gen_model=None):
-    X_data_all = []
-    Y_data_all = []
+def get_all_subject_x_y(data, include_segments=2, augmentation_factor=0):
+    NeedToSearch_X_data_all = []
+    NeedToSearch_Y_data_all = []
+
+    CorrectSearch_X_data_all = []
+    CorrectSearch_Y_data_all = []
+
+    IncorrectSearch_X_data_all = []
+    IncorrectSearch_Y_data_all = []
+
+    Augmented_X_data_all = []
+    Augmented_Y_data_all = []
 
     if include_segments < 2:
         raise ValueError("include_segments must be greater than 1")
@@ -69,14 +83,23 @@ def get_all_subject_x_y(data, include_segments=2, augmentation_factor=0, gen_mod
         Selected_CorrectSearchFeatures = get_selected_features(CorrectSearchFeatures, include_segments)
         Selected_IncorrectSearchFeatures = get_selected_features(IncorrectSearchFeatures, include_segments)
 
-        NeedToSearch_X, NeedToSearch_Y = get_search_x_y(Selected_NeedToSearchFeatures, label=0, augmentation_factor=augmentation_factor, gen_model=gen_model)
-        CorrectSearch_X, CorrectSearch_Y = get_search_x_y(Selected_CorrectSearchFeatures, label=1)
-        IncorrectSearch_X, IncorrectSearch_Y = get_search_x_y(Selected_IncorrectSearchFeatures, label=1)
+        NeedToSearch_X, NeedToSearch_Y = get_search_x_y(Selected_NeedToSearchFeatures, label=0, augmentation_factor=augmentation_factor)
+        CorrectSearch_X, CorrectSearch_Y, = get_search_x_y(Selected_CorrectSearchFeatures, label=1)
+        IncorrectSearch_X, IncorrectSearch_Y,  = get_search_x_y(Selected_IncorrectSearchFeatures, label=1)
 
-        X_data, Y_data = combine_data(NeedToSearch_X, CorrectSearch_X, IncorrectSearch_X, NeedToSearch_Y, CorrectSearch_Y, IncorrectSearch_Y)
-        X_data_all += X_data
-        Y_data_all += Y_data
-    return X_data_all, Y_data_all
+
+
+        #X_data, Y_data = combine_data(NeedToSearch_X, CorrectSearch_X, IncorrectSearch_X, NeedToSearch_Y, CorrectSearch_Y, IncorrectSearch_Y, augmentation_len)
+        NeedToSearch_X_data_all += NeedToSearch_X
+        NeedToSearch_Y_data_all += NeedToSearch_Y
+
+        CorrectSearch_X_data_all += CorrectSearch_X
+        CorrectSearch_Y_data_all += CorrectSearch_Y
+
+        IncorrectSearch_X_data_all += IncorrectSearch_X
+        IncorrectSearch_Y_data_all += IncorrectSearch_Y
+
+    return NeedToSearch_X_data_all, NeedToSearch_Y_data_all, CorrectSearch_X_data_all, CorrectSearch_Y_data_all, IncorrectSearch_X_data_all, IncorrectSearch_Y_data_all
 
 
 def get_metrics(model, X_Test, Y_Test):
@@ -133,6 +156,45 @@ class Generator(nn.Module):
 
         return z
 
+def create_confusion_matrix_plot(model, X_Test, Y_Test, classes,show_plot=False):
+    """
+    Creator : Niall
+
+    Function used to print a confusion matrix and classification report for a given model
+
+    Args:
+        model: the trained model
+        X_Test: the X test data
+        Y_Test: the Y labels for the test data
+        classes: a list containing the class names as string
+
+    Returns:
+        prints the confusion matrix and returns a dictionary containing the classification report
+
+    """
+    y_pred = model.predict(X_Test)
+
+    clr = classification_report(Y_Test, y_pred, target_names=classes)
+    dict_clr = classification_report(Y_Test, y_pred, target_names=classes, output_dict=True)
+
+    cm = confusion_matrix(Y_Test, y_pred)
+
+
+    if show_plot:
+        plt.figure(figsize=(8, 8))
+        sns.heatmap(cm, annot=True, vmin=0, fmt='g', cbar=False, cmap='Blues')
+        plt.tick_params(labelsize=25)
+        plt.xticks(np.arange(len(classes)) + 0.5, classes)
+        plt.yticks(np.arange(len(classes)) + 0.5, classes)
+        plt.xlabel("Predicted", fontdict={'size': 25})
+        plt.ylabel("Actual", fontdict={'size': 25})
+        plt.title("Confusion Matrix", fontdict={'size': 25})
+        plt.savefig("ConfusionMatrix.png")
+        plt.show()
+
+
+    print("Classification Report:\n----------------------\n", clr)
+    return cm,dict_clr
 
 if __name__ == "__main__":
     path = r"C:\Users\gxb18167\PycharmProjects\SIGIR_EEG_GAN\Development\Information-Need\Data\stat_features\Participant_Features.pkl"
@@ -149,11 +211,47 @@ if __name__ == "__main__":
     # Set the model to evaluation mode
     gen_model.eval()
 
-    X, Y = get_all_subject_x_y(data, include_segments=5, augmentation_factor=85, gen_model=gen_model)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
+    NeedToSearch_X_data_all, NeedToSearch_Y_data_all, CorrectSearch_X_data_all, CorrectSearch_Y_data_all, IncorrectSearch_X_data_all, IncorrectSearch_Y_data_all = get_all_subject_x_y(data, include_segments=6)
+
+
+    NeedToSearch_X_train, NeedToSearch_X_test, NeedToSearch_y_train, NeedToSearch_y_test = train_test_split(NeedToSearch_X_data_all, NeedToSearch_Y_data_all, test_size=0.2, random_state=1)
+    CorrectSearch_X_train, CorrectSearch_X_test, CorrectSearch_y_train, CorrectSearch_y_test = train_test_split(CorrectSearch_X_data_all, CorrectSearch_Y_data_all, test_size=0.2, random_state=1)
+    IncorrectSearch_X_train, IncorrectSearch_X_test, IncorrectSearch_y_train, IncorrectSearch_y_test = train_test_split(IncorrectSearch_X_data_all, IncorrectSearch_Y_data_all, test_size=0.2, random_state=1)
+
+
+    NeedToSearch_augmented_X = []
+    NeedToSearch_augmented_Y = []
+
+    #segment 10, augmentation 20
+    augmentation_factor = 0
+    for index in range(floor((len(NeedToSearch_X_train)/100)*augmentation_factor)):
+        synthetic_sample = gs.generate_synthetic_samples(gen_model, NeedToSearch_X_train)
+        NeedToSearch_augmented_X.append(synthetic_sample)
+        NeedToSearch_augmented_Y.append(0)
+
+    NeedToSearch_X_train = NeedToSearch_X_train + NeedToSearch_augmented_X
+    NeedToSearch_y_train = NeedToSearch_y_train + NeedToSearch_augmented_Y
+
+    equal_size_train = floor((len(NeedToSearch_X_train)/2))
+    X_train = NeedToSearch_X_train + CorrectSearch_X_train[:equal_size_train] + IncorrectSearch_X_train[:equal_size_train]
+    y_train = NeedToSearch_y_train + CorrectSearch_y_train[:equal_size_train] + IncorrectSearch_y_train[:equal_size_train]
+
+
+    equal_size_test = floor((len(NeedToSearch_X_test)/2))
+    X_test = NeedToSearch_X_test + CorrectSearch_X_test[:equal_size_test] + IncorrectSearch_X_test[:equal_size_test]
+    y_test = NeedToSearch_y_test + CorrectSearch_y_test[:equal_size_test] + IncorrectSearch_y_test[:equal_size_test]
+
+    print(Counter(y_train))
+    print(Counter(y_test))
+
+
     clf = RandomForestClassifier(max_depth=20, random_state=0)
     clf.fit(X_train, y_train)
 
 
+
+
     get_metrics(clf, X_test, y_test)
+
+    create_confusion_matrix_plot(clf, X_test, y_test, ['0', '1'], show_plot=True)
