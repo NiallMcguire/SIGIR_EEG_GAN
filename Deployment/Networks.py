8835eca07cde7ea56396653dc446bd04d161fb50
NiallMcguire <mundanelarch@gmail.com>
2024-03-20 13:25:33 +0000

    tf-idf, IDF, and TF scores

--- Deployment/Networks.py
+++ Deployment/Networks.py
@@ -803,6 +803,44 @@ class GeneratorDCGAN_v2_Sentence(nn.Module):
 
         return z
 
+class DiscriminatorDCGAN_v2_Sentence(nn.Module):
+    def __init__(self, n_filters, word_embedding_dim):
+        super(DiscriminatorDCGAN_v2_Sentence, self).__init__()
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.word_embedding_dim = word_embedding_dim
+        self.fc_word_embedding = nn.Linear(word_embedding_dim, 105 * 456)
+
+        self.network = nn.Sequential(
+            nn.Conv2d(2, n_filters, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(n_filters, n_filters*2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.InstanceNorm2d(n_filters * 2),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(n_filters*2, n_filters*4, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.InstanceNorm2d(n_filters*4),
+            nn.LeakyReLU(0.2),
+
+            nn.Flatten(),  # Flatten spatial dimensions
+
+            # Fully connected layer to reduce to a single value per sample
+            nn.Linear(n_filters*4 * (105 // 8) * (456 // 8), 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input, word_embedding):
+        word_embedding = self.fc_word_embedding(word_embedding.to(self.device))
+        word_embedding = word_embedding.view(word_embedding.size(0), 1, 105, 456)
+
+
+        combined_input = torch.cat([input, word_embedding], dim=1)
+
+        output = self.network(combined_input)
+        return output
+
 
 class GeneratorWGAN_v1_Sentence(nn.Module):
     def __init__(self, noise_dim, word_embedding_dim):
@@ -876,6 +914,90 @@ class DiscriminatorWGAN_v1_Sentence(nn.Module):
         word_embedding = self.fc_word_embedding(word_embedding.to(self.device))
         word_embedding = word_embedding.view(word_embedding.size(0), 1, 105, 456)
 
+
+        combined_input = torch.cat([input, word_embedding], dim=1)
+
+        output = self.network(combined_input)
+        return output
+
+
+class GeneratorWGAN_v2_Sentence(nn.Module):
+    def __init__(self, noise_dim, word_embedding_dim):
+        super(GeneratorWGAN_v2_Sentence, self).__init__()
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.noise_dim = noise_dim
+        self.word_embedding_dim = word_embedding_dim
+
+        # Define the layers of your generator
+        self.fc_noise = nn.Linear(noise_dim, 105*456)  # Increase the size for more complexity
+        self.fc_word_embedding = nn.Linear(word_embedding_dim, 105*456)  # Increase the size for more complexity
+        self.conv1 = nn.Conv2d(2, 128, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.relu = nn.LeakyReLU(0.2)
+
+        self.conv2 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
+        self.tanh = nn.Tanh()
+
+    def forward(self, noise, word_embedding):
+        # Process noise
+        noise = self.fc_noise(noise)
+        noise = noise.view(noise.size(0), 1, 105,456)  # Adjust the size to match conv1
+
+        # Process word embedding
+        word_embedding = self.fc_word_embedding(word_embedding.to(self.device))
+        word_embedding = word_embedding.view(word_embedding.size(0), 1, 105, 456)  # Adjust the size to match conv1
+
+        # Concatenate noise and word embedding
+        combined_input = torch.cat([noise, word_embedding], dim=1)
+
+        # Upsample and generate the output
+        z = self.conv1(combined_input)
+        z = self.bn1(z)
+        z = self.relu(z)
+
+        z = self.conv2(z)
+        z = self.bn2(z)
+        z = self.relu(z)
+
+        z = self.conv3(z)
+        z = self.tanh(z)
+
+        return z
+
+class DiscriminatorWGAN_v2_Sentence(nn.Module):
+    def __init__(self, n_filters, word_embedding_dim):
+        super(DiscriminatorWGAN_v2_Sentence, self).__init__()
+
+        self.word_embedding_dim = word_embedding_dim
+        self.fc_word_embedding = nn.Linear(word_embedding_dim, 105 * 456)
+
+        self.network = nn.Sequential(
+            nn.Conv2d(2, n_filters, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(n_filters, n_filters*2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.InstanceNorm2d(n_filters * 2),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(n_filters*2, n_filters*4, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.InstanceNorm2d(n_filters*4),
+            nn.LeakyReLU(0.2),
+
+            nn.Flatten(),  # Flatten spatial dimensions
+
+            # Fully connected layer to reduce to a single value per sample
+            nn.Linear(n_filters*4 * (105 // 8) * (456 // 8), 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input, word_embedding):
+        word_embedding = self.fc_word_embedding(word_embedding.to(self.device))
+        word_embedding = word_embedding.view(word_embedding.size(0), 1, 105, 456)
 
         combined_input = torch.cat([input, word_embedding], dim=1)
 
