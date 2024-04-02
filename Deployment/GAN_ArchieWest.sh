e089a1f1a20a88ebb25ecbcc397a425ba8e2b836
NiallMcguire <mundanelarch@gmail.com>
2024-04-02 19:47:16 +0100

    fix

--- Deployment/GAN_ArchieWest.sh
+++ Deployment/GAN_ArchieWest.sh
@@ -42,7 +42,7 @@ module load anaconda/python-3.9.7/2021.11
 
 #Modify the line below to run your program. This is an example
 #=========================================================
-python /users/gxb18167/SIGIR_EEG_GAN/Deployment/Train_GAN.py --model DCGAN_v2_Text --Generation_Size Sentence_Level
+python /users/gxb18167/SIGIR_EEG_GAN/Deployment/Train_GAN.py --model WGAN_v2_Text --Generation_Size Sentence_Level
 
 # Do not change the line below
 #=========================================================
