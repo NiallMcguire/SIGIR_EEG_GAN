c1f31e9b146b8cd90f2cfe83af55ecc21be9511a
NiallMcguire <mundanelarch@gmail.com>
2024-04-17 18:08:37 +0100

    NER

--- Deployment/train_NER_ArchieWest.sh
+++ Deployment/train_NER_ArchieWest.sh
@@ -42,7 +42,7 @@ module load anaconda/python-3.9.7/2021.11
 
 #Modify the line below to run your program. This is an example
 #=========================================================
-python /users/gxb18167/SIGIR_EEG_GAN/Deployment/Named_Entity_Classification.py --model BLSTM_v1 --augmentation_size 0
+python /users/gxb18167/SIGIR_EEG_GAN/Deployment/Named_Entity_Classification.py --model BLSTM_v1 --augmentation_size 5
 
 
 # Do not change the line below
