43df1d9af29944b58d0757ebf053493f3001bb0e
NiallMcguire <mundanelarch@gmail.com>
2024-04-24 10:08:05 +0100

    NER

--- Deployment/Named_Entity_Classification.py
+++ Deployment/Named_Entity_Classification.py
@@ -280,7 +280,7 @@ if __name__ == '__main__':
             label = sampled_labels[i]
             label = label.unsqueeze(0)
             Synthetic_Named_Entity = augment_dataset(gen_model, model_name, word_embeddings,list_of_eeg_segments, Named_Entity)
-            for j in range(len(Synthetic_Named_Entity)):
+            #for j in range(len(Synthetic_Named_Entity)):
 
                 #X_train_numpy = np.append(X_train_numpy, Synthetic_Named_Entity)
                 #y_train_categorical = torch.cat((y_train_categorical, label), dim=0)
